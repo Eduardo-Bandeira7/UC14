102541eb67ad2a68c3549c5dd007836c89998764
Eduardo Bandeira <carloedubf@gmail.com>
2022-10-01 16:47:12 -0300

    entrega das atividades

--- Chapter1FS3/Controllers/LivroController.cs
+++ Chapter1FS3/Controllers/LivroController.cs
@@ -1,4 +1,5 @@
-﻿using Chapter1FS3.Repositories;
+﻿using Chapter1FS3.Models;
+using Chapter1FS3.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -22,6 +23,77 @@ namespace Chapter1FS3.Controllers
             try
             {
                 return Ok(_livroRepository.Listar());
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+        }
+
+
+        [HttpGet("{id}")]
+
+        public IActionResult BuscarPorId(int id)
+        {
+            try
+            {
+                Livro LivroBuscado = _livroRepository.BuscarPorId(id);
+
+                if (LivroBuscado == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(LivroBuscado);
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Livro l)
+        {
+            try
+            {
+                _livroRepository.Cadastrar(l);
+                return StatusCode(201);
+                //return Ok("Cadastro realizado com sucasso!");
+            }
+            catch(Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+        }
+
+
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            try
+            {
+                _livroRepository.Deletar(id);
+                return Ok("Livro deletado");
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+
+        public IActionResult Alterar(int id, Livro l)
+        {
+            try
+            {
+                _livroRepository.Alterar(id, l);
+                return StatusCode(204);
             }
             catch (Exception e)
             {
--- Chapter1FS3/Repositories/LivroRepository.cs
+++ Chapter1FS3/Repositories/LivroRepository.cs
@@ -17,5 +17,39 @@ namespace Chapter1FS3.Repositories
         {
             return _context.Livros.ToList();
         }
+
+        public Livro BuscarPorId(int id)
+        {
+            return _context.Livros.Find(id);
+        }
+
+        public void Cadastrar(Livro l)
+        {
+            _context.Livros.Add(l);
+            _context.SaveChanges();
+        }
+
+        public void Deletar(int id)
+        {
+            Livro l = _context.Livros.Find(id);
+
+            _context.Livros.Remove(l);
+
+            _context.SaveChanges();
+        }
+
+        public void Alterar(int id, Livro l)
+        {
+            Livro LivroBuscado = _context.Livros.Find(id);
+            if (LivroBuscado != null)
+            {
+                LivroBuscado.Titulo = l.Titulo;
+                LivroBuscado.QauntidadePaginas = l.QauntidadePaginas;
+                LivroBuscado.Disponivel = l.Disponivel;
+
+                _context.Livros.Update(LivroBuscado);
+                _context.SaveChanges();
+            }
+        }
     }
 }
